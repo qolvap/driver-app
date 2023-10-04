c671c49b6c85349a6c6b4f4e6fddf8d384862d7f
Pavlo Grebennikov <pavlogrebennikov@MacBook-Air-Pavlo.local>
2023-10-04 22:16:52 +0200

    checking the last changes

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CopyrightNotice: React.FC = () => {
-    const currentYear = new Date().getFullYear(); // Отримуємо поточний рік
+    const currentYear = new Date().getFullYear();
   
     return (
       <div>
